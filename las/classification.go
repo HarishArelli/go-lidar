456d469235bd137f83ca9d4d5853312deaa36a40
Kyle Shannon <kyle@pobox.com>
2015-12-09 13:10:29 -0700

    add header for copyright

--- las/classification.go
+++ las/classification.go
@@ -1,4 +1,11 @@
+// Copyright (c) 2015 Boise Center Aerospace Laboratory.
+// All rights reserved.  // Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+
 package las
+
+// String representations of classification values
 
 var classification05 = [...]string{"Created, never classified",
 	"Unclassified",
