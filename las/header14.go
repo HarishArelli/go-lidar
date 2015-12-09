a9c796376846cbd9d3fc2e3cf9f567f54f5de2ae
Kyle Shannon <kyle@pobox.com>
2015-12-09 14:38:39 -0700

    fix header

--- las/header14.go
+++ las/header14.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2015 Boise Center Aerospace Laboratory.
-// All rights reserved.  // Use of this source code is governed by the MIT
+// All rights reserved.  Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
 
