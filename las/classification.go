028c339b2d582ae4ac4a61dcc44c2176ea0b43f7
Kyle Shannon <kyle@pobox.com>
2015-12-09 14:56:38 -0700

    go fmt

--- las/classification.go
+++ las/classification.go
@@ -2,7 +2,6 @@
 // All rights reserved.  Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
-
 package las
 
 // String representations of classification values
