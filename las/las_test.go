f38a254c63890c7dcc1dd04915ea9efe2ce35d8f
Kyle Shannon <kyle@pobox.com>
2015-12-08 20:07:14 -0700

    add point read test, minimal

--- las/las_test.go
+++ las/las_test.go
@@ -138,7 +138,8 @@ func PointSize(t *testing.T) {
 */
 func TestPointCount(t *testing.T) {
 	l, err := Open("../data/xyzrgb_manuscript_detail.las")
-	if l == nil || err != nil {
+	if err != nil {
+		t.Log(err)
 		t.FailNow()
 	}
 	i := 0
@@ -314,6 +315,92 @@ func TestRawExtents(t *testing.T) {
 	}
 	if YMin != l.header.MinY() {
 		t.Logf("Header y min doesn't match actual(%f != %f)", YMin, l.header.MinY())
+		t.Fail()
+	}
+}
+
+func TestPoint(t *testing.T) {
+	l, err := Open("../data/xyzrgb_manuscript_detail.las")
+	if l == nil || err != nil {
+		t.Log("Failed to open file for reading.")
+		t.FailNow()
+	}
+	p, err := l.GetNextPoint()
+	if err != nil {
+		t.Log(err)
+	}
+	if p.RecordFormat() != 2 {
+		t.Log("Invalid record format for point")
+		t.Fail()
+	}
+	if p.X() < 0 {
+		t.Fail()
+	}
+	if p.Y() < 0 {
+		t.Fail()
+	}
+	if p.Z() < 0 {
+		t.Fail()
+	}
+	if p.Intensity() < 0 {
+		t.Fail()
+	}
+	if p.RetNum() < 0 {
+		t.Fail()
+	}
+	if p.RetCount() < 0 {
+		t.Fail()
+	}
+	if p.ScanFlag() < 0 {
+		t.Fail()
+	}
+	if p.Edge() < 0 {
+		t.Fail()
+	}
+	if p.Classification() < 0 {
+		t.Fail()
+	}
+	//ClassificationString() string
+	if p.ScanAngle() < 0 {
+		t.Fail()
+	}
+	if p.UserData() < 0 {
+		t.Fail()
+	}
+	if p.PointSourceID() < 0 {
+		t.Fail()
+	}
+	if p.GpsTime() < 0 {
+		t.Fail()
+	}
+	if p.Red() < 0 {
+		t.Fail()
+	}
+	if p.Green() < 0 {
+		t.Fail()
+	}
+	if p.Blue() < 0 {
+		t.Fail()
+	}
+	if p.NIR() < 0 {
+		t.Fail()
+	}
+	if p.WavePacketDesc() < 0 {
+		t.Fail()
+	}
+	if p.WaveOffset() < 0 {
+		t.Fail()
+	}
+	if p.WaveSize() < 0 {
+		t.Fail()
+	}
+	if p.X_t() < 0 {
+		t.Fail()
+	}
+	if p.Y_t() < 0 {
+		t.Fail()
+	}
+	if p.Z_t() < 0 {
 		t.Fail()
 	}
 }
